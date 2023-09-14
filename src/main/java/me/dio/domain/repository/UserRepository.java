3835af1633f01aeb0851ab7017b5cfb8c2a33199
riclovato <riic.lovato@gmail.com>
2023-09-14 18:36:40 -0300

    Camada de serviço

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -2,6 +2,9 @@ package me.dio.domain.repository;
 
 import me.dio.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+        boolean existsByAccountNumber(String accountNumber);
 }
